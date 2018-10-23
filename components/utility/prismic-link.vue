c084725ee68ac29af5bde9f7c4ab3d0a55296987
Colin Baxter <colin.baxter@zaneray.com>
2018-10-23 12:43:31 -0600

    Updating the prism link component to handle target settings from the CMS

--- components/utility/prismic-link.vue
+++ components/utility/prismic-link.vue
@@ -2,7 +2,7 @@
   <nuxt-link class="pr" :to="$prismic.asLink(link)" v-if="link.link_type === 'Document'">
     <slot></slot>
   </nuxt-link>
-  <a :href="$prismic.asLink(link)" v-else-if="link.link_type === 'Web'">
+  <a :href="$prismic.asLink(link)" :target="linkTarget" v-else-if="link.link_type === 'Web'">
     <slot></slot>
   </a>
   <nuxt-link :to="link" v-else>
@@ -12,7 +12,12 @@
 
 <script>
   export default {
-    props: ['link']
+    props: ['link'],
+    computed: {
+      linkTarget() {
+          return this.link.target === '_blank' ? '_blank' : '_self'
+      }
+    }
   }
 </script>
 
