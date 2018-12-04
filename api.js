f5833d5216471537e68de2a7f8906bba1af2e0a0
Colin Baxter <colin.baxter@zaneray.com>
2018-12-04 11:37:22 -0700

    Updating service workers in project

--- api.js
+++
@@ -1,6 +0,0 @@
-export const appEndpoints = {
-  prismic: {
-    apiEndpoint: 'https://vue-demo.cdn.prismic.io/api/v2',
-    accessToken: 'MC5XOEZxSkJFQUFFOEVyd1lu.L--_ve-_vQrvv71kc0rvv70S77-977-9eXgDGu-_ve-_ve-_ve-_ve-_ve-_ve-_ve-_ve-_ve-_vWEP77-9Pu-_ve-_vQ'
-  }
-}
