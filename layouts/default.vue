26cf082e8d9dd9c2ef4861d326694b12fe284b0f
Colin Baxter <colin.baxter@zaneray.com>
2018-12-04 11:26:06 -0700

    Testing new sw strategy

--- layouts/default.vue
+++ layouts/default.vue
@@ -32,7 +32,7 @@
         return this.footerLinks ? this.footerLinks.data.nav : null
       }
     },
-    beforeMount() {
+    mounted() {
       //Register service worker globally
       if ('serviceWorker' in navigator) {
         window.addEventListener('load', function() {
