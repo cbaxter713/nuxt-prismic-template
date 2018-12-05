82e9617c09c94312a9f4c8586e88bfb17b3aa41a
Colin Baxter <colin.baxter@zaneray.com>
2018-12-05 00:14:23 -0700

    Creating a TextBlock slice to support the Text slice in prismic, and adding some basic formatting around that

---
+++ components/slices/TextBlock.vue
@@ -0,0 +1,29 @@
+<template>
+    <section class="text-block container">
+        <div class="text-block-content" v-html="$prismic.dom.RichText.asHtml(fields.text)"></div>
+    </section>
+</template>
+
+<script>
+  export default {
+    name: "TextBlock",
+    props: [
+      'fields'
+    ],
+    mounted() {
+      console.log('text, ', this.fields);
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+    .text-block {
+        padding-top: $margin-large;
+        padding-bottom: $margin-large;
+    }
+
+    .text-block-content {
+        max-width: $content-width;
+        margin: 0 auto;
+    }
+</style>
